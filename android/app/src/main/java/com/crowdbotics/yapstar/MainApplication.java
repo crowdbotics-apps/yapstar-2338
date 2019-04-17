89ac9b3d4b97e317595b3732b48c6f8f6a685844
webmaster <webmasterdragon@outlook.com>
2019-04-17 10:00:34 +0700

    rename project

--- android/app/src/main/java/com/crowdbotics/yapstar/MainApplication.java
+++ android/app/src/main/java/com/crowdbotics/yapstar/MainApplication.java
@@ -1,4 +1,4 @@
-package com.invertase.rnfirebasestarter;
+package com.crowdbotics.yapstar;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
---
+++ ios/Pods/Target Support Files/Pods-Yapstar/Pods-Yapstar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Yapstar : NSObject
+@end
+@implementation PodsDummy_Pods_Yapstar
+@end
