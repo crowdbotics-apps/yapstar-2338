881746400667094350e5f92f4582a07e001bac30
webmaster <webmasterdragon@outlook.com>
2019-04-25 14:19:09 +0700

    login screen design

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { View, Image, NativeModules, Text, ScrollView } from 'react-native';
+import {
+  View,
+  Image,
+  NativeModules,
+  Text,
+  SafeAreaView,
+  ImageBackground
+} from 'react-native';
 import PropTypes from 'prop-types';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-
 import { AccessToken, LoginManager } from 'react-native-fbsdk';
 import firebase from 'react-native-firebase';
 import InstagramLogin from 'react-native-instagram-login';
-
 import { AuthController } from 'app/services';
 import { AppContext, Button } from 'app/components';
 import { alert } from 'app/utils/Alert';
 
 import styles from './style';
-import LogoIcon from 'app/assets/images/logo.png';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+const IMAGE_LOGO = require('app/assets/images/app_logo.png');
+const IMAGE_BACKGROUND = require('app/assets/images/background.png');
+const ICON_FB = require('app/assets/images/facebook.png');
+const ICON_TW = require('app/assets/images/twitter.png');
+const ICON_GL = require('app/assets/images/instagram.png');
 
 const { RNTwitterSignIn } = NativeModules;
 const { TwitterAuthProvider } = firebase.auth;
@@ -92,7 +102,6 @@ class LoginScreen extends React.Component {
 
     RNTwitterSignIn.logIn()
       .then((loginData) => {
-        console.log(loginData);
         const { authToken, authTokenSecret } = loginData;
         if (authToken && authTokenSecret) {
           this.setState({
@@ -103,7 +112,8 @@ class LoginScreen extends React.Component {
       })
       .then((credential) => {
         console.log('firebase auth credential', credential);
-        return firebase.auth().signInWithCredential(credential);
+        firebase.auth().signInWithCredential(credential);
+        return this.props.navigation.navigate('main');
       })
       // .then((firebaseUserCredential) => {
       //   console.log(JSON.stringify(firebaseUserCredential.user.toJSON()));
@@ -114,58 +124,79 @@ class LoginScreen extends React.Component {
       });
   };
 
+  googleLogin() {}
+
   render() {
     return (
-      <ScrollView>
+      <ImageBackground source={IMAGE_BACKGROUND} style={styles.background}>
         <KeyboardAwareScrollView contentContainerStyle={styles.container}>
-          <View style={styles.container}>
-            <Image source={LogoIcon} style={styles.logo} resizeMode="contain" />
-            <View style={styles.content}>
-              <Button
-                containerStyle={styles.loginBtn}
-                textStyle={styles.login}
-                text="Facebook"
-                onPress={this.facebookLogin}
-              />
-              <Button
-                containerStyle={styles.loginBtn}
-                textStyle={styles.login}
-                text="Twitter"
-                onPress={this.twitterLogin}
-              />
-              <Button
-                containerStyle={styles.loginBtn}
-                textStyle={styles.login}
-                text="Instagram"
-                onPress={() => this.instagramLogin.show()}
-              />
-              <Button
-                containerStyle={styles.forgotpswdBtn}
-                textStyle={styles.forgotpswd}
-                text="Forgot password?"
-                onPress={this.goToForgotpswd}
-              />
-              <View style={styles.signupContainer}>
-                <Text style={styles.description}>Do not have an account? </Text>
-                <Button
-                  containerStyle={styles.signupBtn}
-                  textStyle={styles.signup}
-                  text="Sign Up"
-                  onPress={this.goToSignUp}
+          <SafeAreaView style={styles.container}>
+            <View style={styles.container}>
+              <View style={styles.content}>
+                <Image
+                  source={IMAGE_LOGO}
+                  style={styles.logo}
+                  resizeMode="contain"
                 />
               </View>
+              <View style={styles.content}>
+                <Text style={styles.desc}>
+                  Sign In with your social media acoount
+                </Text>
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity
+                    onPress={() => this.facebookLogin()}
+                    style={styles.button}
+                  >
+                    <Image
+                      source={ICON_FB}
+                      style={styles.btnImg}
+                      resizeMode="contain"
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => this.twitterLogin()}
+                    style={styles.button}
+                  >
+                    <Image
+                      source={ICON_TW}
+                      style={styles.btnImg}
+                      resizeMode="contain"
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => this.googleLogin()}
+                    style={styles.button}
+                  >
+                    <Image
+                      source={ICON_GL}
+                      style={styles.btnImg}
+                      resizeMode="contain"
+                    />
+                  </TouchableOpacity>
+                </View>
+
+                <View style={styles.signupContainer}>
+                  <TouchableOpacity onPress={() => this.goToSignUp()}>
+                    <Text style={styles.description}>
+                      Do not have an account?{' '}
+                      <Text style={styles.signup}>Sign up</Text>
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+              {/* <InstagramLogin
+                ref={(ref) => (this.instagramLogin = ref)}
+                clientId="5d40831591bc467d835d3bfa61884cf4"
+                redirectUrl="http://bolthill.com"
+                scopes={['public_content', 'follower_list']}
+                onLoginSuccess={(token) => this.setState({ token })}
+                onLoginFailure={(data) => console.log(data)}
+              /> */}
             </View>
-            <InstagramLogin
-              ref={(ref) => (this.instagramLogin = ref)}
-              clientId="5d40831591bc467d835d3bfa61884cf4"
-              redirectUrl="http://bolthill.com"
-              scopes={['public_content', 'follower_list']}
-              onLoginSuccess={(token) => this.setState({ token })}
-              onLoginFailure={(data) => console.log(data)}
-            />
-          </View>
+          </SafeAreaView>
         </KeyboardAwareScrollView>
-      </ScrollView>
+      </ImageBackground>
     );
   }
 }
